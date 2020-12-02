c9af227307df9d869232424b669b64cfe04d3112
Ben Hedgepeth <benhh@gmx.com>
2020-12-01 19:21:48 -0800

    Refactored initial card dealing loop

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 
+
 from classes import Dealer, Player, Hand
+from utils import cards
 
 class BlackjackTable:
 
     def __init__(self):
+        self.cards = cards()
         self.dealer = Dealer()
+        self.players = [self.dealer]
+        self.i = 0
+
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+
+        if self.i < len(self.players):
+            person = self.players[self.i]
+            self.i += 1
+            return person
+        else:
+            raise StopIteration()
 
 
 def main():
     blackjack_table = BlackjackTable()
-    player = Player()
+    while True:
+        for i in range(2):
+            for player in blackjack_table:
+                if i == 1:
+                    cards = []
+                dealt_card = blackjack_table.dealer.deal(self.cards)
+                cards.append(card)
+                if i == 2:
+                    hand = Hand(cards)
+                    player.hands.append(hand)
 
-    player_cards, dealer_cards = blackjack_table.dealer.deal(self.cards)
-    if player_cards[0] == player_cards[1]:
-        while True:
-            try:
-                double_down = input("Would you like to double_down? ").upper()
-            except:
-                pass
-                continue
-            else:
-                if double_down == "Y":
-                    player_cards = blackjack_table.dealer.deal(
-                        self.cards, player_cards, True
-                    )
-                    break
-                
+    player_hand_status = person.check_hand(hand)
