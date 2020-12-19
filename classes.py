38bb033b2bf8e49a8f36912daa1a6b1abae80941
Ben Hedgepeth <benhh@gmx.com>
2020-12-19 00:19:54 -0800

    Switched for loop to while loop to evaluate hands

--- classes.py
+++ classes.py
@@ -1,165 +1,53 @@
 
-from exceptions import LowChipsException
-
-class Dealer:
-
-    def __init__(self, chips=50000):
-        self.chips = 50000
-        self.name = "Dealer"
-        self.hands = []
-
-    def deal(self, cards):
-        card = cards.pop(0)
-        return card
-
-    def __str__(self):
-        return f"{self.name}"
-
-    def __repr__(self):
-        return
-
-
-class Player:
-
-    def __init__(self):
-        self.chip_stack = 0
-        self.bets_made = 0
-        self.hands = []
-        self.name = 'Player'
-
-    def __str__(self):
-        return f"{self.name}"
-
-    def bet(self):
-        existing_bet = getattr(self, 'bets_made')
-        if existing_bet and self.chip_stack:
-            self.chip_stack -= existing_bet
-            if self.chip_stack < 0:
-                self.chip_stack += existing_bet
-                raise LowChipsException("Your bet exceeds your current chip standing!")
-            self.bets_made += self.bets_made
-        elif not existing_bet and self.chip_stack:
-            '''Represents the inital bet before cards are dealt'''
-            while True:
-                try:
-                    bet_placed = abs(int(input("Please place your bet: ")))
-                    if not bet_placed:
-                        print("Cannot accept the bet placed!")
-                        continue
-                except ValueError:
-                    print("Cannot accept the bet placed!")
-                    continue
-                else:
-                    self.chip_stack -= bet_placed
-                    self.bets_made += bet_placed
-                    return
-        else:
-            raise LowChipsException("You have no chips remaining!")
-
-    def check_hand(self, dealer):
-        hand_values = map(lambda hand: hand.value, self.hands)
-        for i, hand in enumerate(self.hands):
-            print(f"{str(self.hands[i])}\n" + '-' * len(str(self.hands[i])) + "\n" + f"Total hand: {hand.value}")
-
-        if (len(self.hands) == 1 and
-        all(card.pip == 11 for card in self.hands[0].cards) or
-        all(card.pip == 8 for card in self.hands[0].cards)):
-            print("Split the cards...")
-            self.bet()
-            cards, position = self.split_hand()
-            return (cards, position)
-
-        while True:
-            position = input("Which play would you like to perform? ").upper()
-            if position == "SPLIT":
-                if len(self.hands) == 2:
-                    print("Can only split your hand one time!")
-                else:
-                    try:
-                        self.bet()
-                        hands, position = self.split_hand()
-                    except ValueError:
-                        print("""
-                            The cards dealt to you don't have the same pip value.
-                            You can only 'HIT', 'STAND', or 'DOUBLE DOWN'.
-                        """)
-                    except LowChipsException as e:
-                        print(e)
-                    else:
-                        return (hands, "SPLIT")
-            elif position == "DOUBLE DOWN":
-                try:
-                    self.bet()
-                except LowChipsException as e:
-                    print(e)
-                else:
-                    hands = self.hands
-                    return (hands, "DOUBLE_DOWN")
-            elif position == "STAND":
-                hands, position = self.stand()
-            else:
-                pass
-
-    def split_hand(self):
-        cards = self.hands[0].cards
-        if cards[0] != cards[1]:
-            raise ValueError
-        hands = []
-        for card in cards:
-            hands.append(Hand([card]))
-        self.hands = hands
-        return (self.hands, "SPLIT")
-
-    # def stand(self):
-    #
-    #     ace_exists = list(filter(lambda hand: any(hasattr(card, 'face_card') and card.face_card == 'Ace' for card in hand.cards), self.hands))
-    #
-    #     if ace_exists:
-    #             try:
-    #                 ace_value = abs(int(input("Do you want your 'Ace(s)' to be worth 1 or 11 pips")))
-    #             except ValueError:
-    #                 print("Invalid pip value for your 'Ace(s)'")
-    #             else:
-    #                 for hand in self.hands:
-    #                     for card in hand.cards:
-    #                         if hasattr(card, 'face_card') and card.face_card == 'Ace':
-    #                             card.pip = ace_value
-    #                 hands = self.hands
-    #     return (hands, "STAND")
-
-
-
-
+from exceptions import BettingError, HandSplitError
 
 
 class Card:
-
     def __init__(self, pip, suit, *args, **kwargs):
         self.pip = pip
         self.suit = suit
-        if kwargs.get('face_card', None):
+        if kwargs and 'face_card' in kwargs:
             self.face_card = kwargs['face_card']
 
-    def __repr__(self):
-        if hasattr(self, "face_card"):
+    def __str__(self):
+        if hasattr(self, 'face_card'):
             return f"{self.face_card} of {self.suit}"
         return f"{self.pip} of {self.suit}"
+
+    def __repr__(self):
+        props = f"({self.pip}, {self.suit}"
+        if not hasattr(self, 'face_card'):
+            return f"{self.__class__.__name__}{props})"
+        props += f", face_card={self.face_card})"
+        return f"{self.__class__.__name__}{props}"
+
 
     def __eq__(self, other):
         return self.pip == other.pip
 
 
-class Hand:
 
+class Hand:
     def __init__(self, cards):
         self.cards = cards
-        self.value = sum([card.pip for card in self.cards])
+        self._value = sum(card.pip for card in cards)
 
     def __str__(self):
-        return "".join([str(card) + "\n" for card in self.cards])
+        return "".join(
+            [f"{str(card)}, " if i == 0 else f"{str(card)}"
+            for i, card in enumerate(self.cards)]
+        )
 
     def __repr__(self):
-        return "".join([f"{card}, " if i == 0 else f"{card}" for i, card in enumerate(self.cards)])
+        card_string = '(['
+        for i, card in enumerate(self.cards):
+            if i != len(self.cards) - 1:
+                card_string += f"Card({card.pip}, {card.suit}), "
+            else:
+                card_string += f"Card({card.pip}, {card.suit})"
+        card_string += '])'
+
+        return f"{self.__class__.__name__}{card_string}"
 
     def __len__(self):
         return len(self.cards)
@@ -167,8 +55,188 @@ class Hand:
     def __eq__(self, other):
         return self.value == other.value
 
-    def __ge__(self, other):
-        return self.value >= other.value
+    def __gt__(self, other):
+        return self.value > other.value
 
-    def __le__(self, other):
-        return self.value <= other.value
+    def __lt__(self, other):
+        return self.value < other.value
+
+    @property
+    def value(self):
+        return sum(card.pip for card in self.cards)
+
+    @value.setter
+    def value(self, value):
+        self._value = value
+
+
+    @property
+    def soft(self):
+        return any(
+            hasattr(card, 'face_card') and
+            card.face_card == "Ace" for card in self.cards
+        )
+
+
+class Dealer:
+    def __init__(self):
+        self.name = "Dealer"
+        self.hands = []
+
+    def __str__(self):
+        return self.name
+
+    def deal_hands(self, cards, player):
+
+        dealt_cards = {f"{player}": [], f"{self}": []}
+        for i in range(2):
+            for person in (player, self):
+                dealt_card = self.deal_card(cards)
+                dealt_cards[f"{person}"].append(dealt_card)
+                if i == 1:
+                    hand = Hand(dealt_cards[f"{person}"])
+                    person.hands.append(hand)
+        return [self.hands[0], player.hands[0]]
+
+    def deal_card(self, cards):
+        card = cards.pop(0)
+        return card
+
+
+class Player:
+
+    def __init__(self):
+        self.name = "Player"
+        self.hands = []
+        self.chips = 0
+        self._bet = 0
+
+    def __str__(self):
+        return self.name
+
+    def bet(self):
+
+        if not self.chips:
+            raise BettingError("You have no chips remaining...")
+        if self._bet:
+            self.chips -= self._bet
+            if (self.chips < 0):
+                self.chips += self._bet
+                raise BettingError(
+                    "Your bet cannot be fulfilled due to inadequate chips."
+                )
+            self._bet += self._bet
+        else:
+            while True:
+                try:
+                    bet_placed = abs(int(input("Place your bet: ")))
+                except ValueError:
+                    print("Cannot accept the subject bet. Try again.")
+                    continue
+                else:
+                    if not bet_placed:
+                        print("Cannot accept the subject bet. Try again.")
+                    else:
+                        self.chips -= bet_placed
+                        self._bet += bet_placed
+                        break
+
+    def split_hand(self, hand):
+
+        cards = hand.cards
+        if len(self.hands) > 1:
+            raise ValueError("Cannot split hands past your initial hand.")
+        elif cards[0] != cards[1]:
+            raise HandSplitError(
+                "Cannot split any cards where their pip values aren't equal."
+            )
+        hands = [Hand([card, ]) for card in cards]
+        self.hands = hands
+        return self.hands
+
+    def check_hand(self, dealer_hand, player_hand):
+
+        print(f"""
+            Chips: {self.chips} - Bet: {self._bet} chips - Hand Score: {player_hand.value}\n
+        """)
+        str_hand1 = list(map(lambda x: len(str(x)), player_hand.cards))
+        str_hand2 = list(map(lambda x: len(str(x)), dealer_hand.cards))
+        max_str_length = max(str_hand1 + str_hand2) + 5
+
+        card_strings = []
+        for hand in (player_hand, dealer_hand):
+            if hand is player_hand:
+                string = f"\t\t\t{('-' * max_str_length)}\n\t\t\tPlayer\n\t\t\t{'=' *  max_str_length}"
+            else:
+                string = f"\t\t\t{('-' * max_str_length)}\n\t\t\tDealer\n\t\t\t{'=' *  max_str_length}"
+            for i, card in enumerate(hand.cards):
+                string += f"\n\t\t\t"
+                if i == 1 and hand is dealer_hand:
+                    string += f"\n\t\t\t{'Down card'.center(max_str_length)}"
+                else:
+                    string += f"\n\t\t\t{str(card).center(max_str_length)}"
+            string += f"\n\t\t\t{'-' * max_str_length}\n"
+            card_strings.append(string)
+
+        card_strings = ''.join(card_strings)
+        print(card_strings)
+
+        while True:
+            play = input("How do you want to play your hand? ").upper()
+            if play not in ['DOUBLE DOWN', "SPLIT", "HIT", "STAND"]:
+                print("That play is not possible. Try another play.")
+                continue
+            if play == "SPLIT" or play == "DOUBLE DOWN":
+                try:
+                    self.bet()
+                except BettingError as e:
+                    print(e)
+                    continue
+                else:
+                    if play == "SPLIT":
+                        try:
+                            hands = self.split_hand(player_hand)
+                            self.hands = hands
+                            return self.hands, "SPLIT"
+                        except HandSplitError as e:
+                            print(e)
+                            self._bet /= 2
+                            self.chips += self._bet
+                            continue
+                    elif play == "DOUBLE DOWN":
+                        try:
+                            hands = self.double_down(player_hand)
+                        except ValueError as e:
+                            print(e)
+                            self._bet /= 2
+                            self.chips += self._bet
+                            continue
+                        return play
+            elif play == "HIT":
+                position = self.hit(player_hand)
+                return position
+            return "STAND"
+
+    def double_down(self, hand):
+        if len(hand.cards) >= 3:
+            raise ValueError("You cannot double down with your present hand")
+        return [hand, ], "DOUBLE DOWN"
+
+    def hit(self, hand):
+        if hand.soft:
+            x, hand = list(filter(
+                lambda h: h[1].soft, enumerate(self.hands)
+            ))[0]
+            total_aces = len(
+                list(
+                    filter(lambda c: hasattr(c, 'face_card') and
+                    c.face_card == "Ace", self.hands[x].cards)
+                )
+            )
+            reduced_hand = self.hands[x].value - (10 * (total_aces - 1))
+            self.hands[x].value = reduced_hand
+            if self.hands[x].value > 21:
+                return self.hands[x], "BUST"
+        if hand.value > 21:
+            return "BUST"
+        return "HIT"
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import itertools
 from classes import Dealer, Player, Hand
 from utils import cards, collect_chips
 
-from exceptions import LowChipsException
+from exceptions import BettingError
 logging.basicConfig(level=logging.INFO)
 
 class BlackjackTable:
@@ -14,27 +14,15 @@ class BlackjackTable:
     def __init__(self):
         self.card_stack = cards()
         self.dealer = Dealer()
-        self.players = [self.dealer]
-
-    def __iter__(self):
-        self.i = 0
-        return self
-
-    def __next__(self):
-
-        if self.i < len(self.players):
-            person = self.players[self.i]
-            self.i += 1
-            return person
-        else:
-            raise StopIteration()
+        self.player = None
 
 
 def main():
     blackjack_table = BlackjackTable()
     player = Player()
-    blackjack_table.players.insert(0, player)
-    player.chip_stack = collect_chips()
+    blackjack_table.player = player
+    print("Welcome to the Blackjack Table! Let's get started!")
+    player.chips = collect_chips()
     while True:
         shuffle(blackjack_table.card_stack)
         try:
@@ -42,37 +30,72 @@ def main():
         except ValueError as e:
             print(e)
             continue
-        except LowChipsException:
-            print("No chips!")
+        except BettingError as e:
+            print(e)
             continue
         else:
-            dealt_cards = {'Player': [], 'Dealer': []}
-            for i in range(2):
-                for person in blackjack_table:
-                    dealt_card = blackjack_table.dealer.deal(blackjack_table.card_stack)
-                    dealt_cards[f"{person}"].append(dealt_card)
-                    if i == 1:
-                        person.hands.append(Hand(dealt_cards[f"{person}"]))
-
-
-        logging.info("Hands have been dealt to both the Player and the Dealer")
-        hands, position = player.check_hand(blackjack_table.dealer)
-
-        while position != "STAND":
-            if position == "DOUBLE_DOWN":
-                dealt_card = blackjack_table.dealer.deal(blackjack_table.card_stack)
-                player.hands[0].cards.append(dealt_card)
-                break
-            elif position == "SPLIT":
-                for i, hand in enumerate(hands):
-                    dealt_card = blackjack_table.dealer.deal(blackjack_table.card_stack)
+            dealer_hand, player_hand = blackjack_table.dealer.deal_hands(
+                blackjack_table.card_stack,
+                player
+            )
+            logging.info(
+                "Hands have been dealt to both the Player and the Dealer."
+            )
+        player_hands = player.hands.copy()
+        i = 0
+        while i < len(player.hands):
+            '''See Note (1).'''
+            position = player.check_hand(blackjack_table.dealer.hands[0], player.hands[i])
+            while position != "BUST":
+                if position == "SPLIT":
+                    for i, hand in enumerate(player.hands):
+                        dealt_card = blackjack_table.deal_card(
+                            blackjack_table.card_stack
+                        )
+                        player.hands[i].cards.append(deal_card)
+                        i += 1
+                    i = 0
+                    break
+                elif position == "HIT" or position == "DOUBLE DOWN":
+                    dealt_card = blackjack_table.dealer.deal_card(
+                        blackjack_table.card_stack
+                    )
                     player.hands[i].cards.append(dealt_card)
-                hands, position = player.check_hand(blackjack_table.dealer)
-            elif posiiton == "HIT":
-                dealt_card = blackjack_table.dealer.deal(blackjack_table.card_stack)
-                hands, position = player.check_hand(blackjack_table.dealer)
-            else:
-                break
+                    position = player.check_hand(blackjack_table.dealer.hands[0], player.hands[i])
+                    continue
+                    if position == "DOUBLE DOWN":
+                        i += 1
+                        break
+                elif position == "STAND":
+                    i += 1
+                    break
+            del player_hands[i]
+            i += 1
+            continue
+        if not player_hands:
+            print("HERE")
+    # if all(hand.value > 21 for hand in final_hands):
+    #     print(f"""
+    #         Dealer wins the round.
+    #         You lost {player._bet} chips.
+    #     """)
+    #     all_dealt_hands = blackjack_table.dealer.hands + final_hands
+    #     for hand in all_dealt_hands:
+    #
+    #         for card in hand.cards:
+    #
+    #     blackjack_table.dealer.chips += player._bet
+    #     player._bet = 0
+    # print("You win")
+
+'''(1): The initial hand was being passed to player.check_hand() on
+two different lines in the source code when implementing a for loop.
+If a player received a hand that could not be split, the second call
+to the initial hand would occur when evaluating to hit, stand, or double down.
+
+If the player is unable to split their cards, player.check_hand() was being
+called on the same hand twice
+'''
 
 if __name__ == "__main__":
     main()
